21746266db49ddddb775cd410f6285f03d7c5143
Mauro A Flores A <mauroa_99@hotmail.com>
2016-08-03 19:17:37 -0500

    Initial Commit

---
+++ PizzaWatchTarea WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PizzaWatchTarea WatchKit Extension
+//
+//  Created by Mauro Alberto Flores Almonte on 03/08/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ PizzaWatchTarea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaWatchTarea
+//
+//  Created by Mauro Alberto Flores Almonte on 03/08/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
